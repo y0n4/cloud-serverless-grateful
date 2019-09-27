d6e1650156f31ea7b4c9a102abd19f5d597e232e
y0n4 <04l.yona@gmail.com>
2019-09-26 23:53:37 -0400

    Rename file

--- backend/src/lambda/http/generateUrl.ts
+++ backend/src/lambda/http/generateUrl.ts
@@ -1,14 +1,14 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
-import { generateUploadUrl } from '../../businessLogic/todos';
+import { generateUrl } from '../../businessLogic/todos';
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
 
   console.log(todoId);
   // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  const uploadUrl = await generateUploadUrl(event)
+  const uploadUrl = await generateUrl(event)
   return {
     statusCode: 200,
     headers: {
