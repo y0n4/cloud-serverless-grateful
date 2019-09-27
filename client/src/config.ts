3669b218c064f3846f77cf36327b96f60ab6ef02
y0n4 <04l.yona@gmail.com>
2019-09-27 00:02:58 -0400

    Configure client codes generated from Auth0

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it ✅
-const apiId = '0v1spzg1ib'
+const apiId = 'nkdvcowthf' // result of "successful sls deploy -v" on backend
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
   domain: 'dev-yfizc0yr.auth0.com',            // Auth0 domain
-  clientId: 'OFN1luy3JzNDSr0I5z6I1VgCdg6FHylt',          // Auth0 client id
+  clientId: 'nrsER55D4nnfBYRa8DSzn2sb5mV6Y24y',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
